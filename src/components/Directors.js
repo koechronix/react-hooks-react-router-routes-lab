ae8d0fb4bfa25025c1dae80cce97f5682e450511
koechronix <koechronix@yahoo.com>
2022-07-19 23:30:02 -0400

    complete

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Directors Page</h1>
+    {directors.map((director, index) => {
+      return <div key={index}>
+        <h3>Name: {director.name}</h3>
+        Movies: <ul>
+          {director.movies.map((movies, index)=>{
+            return <li key={index}>{movies}</li>
+          })}
+        </ul>
+
+      </div>
+    })}
+  </div>;
 }
 
 export default Directors;
